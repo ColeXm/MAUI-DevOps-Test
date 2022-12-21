70f3bb40abaf3f4243608e9e1c16c7cbbd920119
REDMOND\colexia <colexia@microsoft.com>
2022-12-21 18:16:03 +0800

    first commit

---
+++ obj/Debug/net6.0-android/android/src/crc647d5602c67b9b8d64/MainActivity.java
@@ -0,0 +1,46 @@
+package crc647d5602c67b9b8d64;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("MAUISample.MainActivity, MAUISample", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("MAUISample.MainActivity, MAUISample", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("MAUISample.MainActivity, MAUISample", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
